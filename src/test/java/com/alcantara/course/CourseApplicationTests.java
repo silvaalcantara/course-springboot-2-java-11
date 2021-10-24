da29722a87889f4748e1319252b3e5bbcbd8f81d
Allan Alcantara <allan.pa.br@gmail.com>
2021-10-24 16:22:44 -0300

    Criacao do projeto

---
+++ src/test/java/com/alcantara/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alcantara.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
